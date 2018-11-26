3e092f176983e669662eaf742b95fbb0ed99554a
Fei-Fei Zheng <feifeizheng@outlook.com>
2018-11-25 22:45:12 -0500

    calculate real world curvature

--- project/detection.py
+++ project/detection.py
@@ -90,3 +90,21 @@ def find_lane(binary_warped, peak, prev_left=[], prev_right=[]):
 
     return left_fit, right_fit, left_fitx, right_fitx, leftx, lefty, rightx, righty, ploty
 
+def compute_curvature(left_fit, right_fit, left_fitx, right_fitx, ploty):
+    y_eval = np.max(ploty)
+
+    leftx = left_fitx
+    rightx = right_fitx
+
+    ym_per_pix = 30/720 # meters per pixel in y dimension
+    xm_per_pix = 3.7/700 # meters per pixel in x dimension
+
+    # Fit new polynomials to x,y in world space
+    left_fit_cr = np.polyfit(ploty * ym_per_pix, leftx * xm_per_pix, 2)
+    right_fit_cr = np.polyfit(ploty * ym_per_pix, rightx * xm_per_pix, 2)
+
+    # Calculation of R_curve (radius of curvature)
+    left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+    right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+
+    return left_curverad, right_curverad
--- project/main.py
+++ project/main.py
@@ -28,38 +28,38 @@ right = []
 
 for i in range(len(warped_images)):
     im = warped_images[i]
-    
     if i == 0: 
         left_fit, right_fit, left_fitx, right_fitx, leftx, lefty, rightx, righty, ploty = dt.find_lane(im, peaks[i], [], [])
     else:
         left_fit, right_fit, left_fitx, right_fitx, leftx, lefty, rightx, righty, ploty = dt.find_lane(im, peaks[i], prev_left[i-1], prev_right[i-1])
 
-    out_img = np.dstack((im, im, im))
-    out_img[lefty, leftx] = [255, 0, 0]
-    out_img[righty, rightx] = [0, 0, 255]
-    result.append(out_img)
+    lc, rc = dt.compute_curvature(left_fit, right_fit, left_fitx, right_fitx, ploty)
 
     left.append([leftx, lefty])
     right.append([rightx, righty])
     left_fitxx.append(left_fitx)
     right_fitxx.append(right_fitx)
-    # left_fit, right_fit, ploty, out_img = cur.fit_polynomial(im)
-    # lc, rc = cur.measure_curvature_pixels(ploty, left_fit, right_fit)
+
     prev_left.append(left_fit)
     prev_right.append(right_fit)
 
-    # left_c.append(lc)
-    # right_c.append(rc)
-    # print('lc: ', lc, '|', 'rc: ', rc)
+    left_c.append(lc)
+    right_c.append(rc)
+    print('lc: ', lc, '|', 'rc: ', rc)
 
     # title = "Lane of Image " + str(i+1)
     # plt.imshow(out_img)
     # plt.title(title)
     # plt.show()
 
-
 '''======================================================='''
 # plot images
+
+    # out_img = np.dstack((im, im, im))
+    # out_img[lefty, leftx] = [255, 0, 0]
+    # out_img[righty, rightx] = [0, 0, 255]
+    # result.append(out_img)
+
 # fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, figsize=(7, 10))
 
 # plt.subplot(221)
